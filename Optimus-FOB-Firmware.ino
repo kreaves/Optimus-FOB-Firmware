62aea938c47cb7c933e570a0bb0bb695cd002103
Jason Melton <jmelton@jasonmelton.com>
2021-09-29 17:49:07 -0400

    Add initial Arduino file

---
+++ Optimus-FOB-Firmware.ino
@@ -0,0 +1,40 @@
+///
+/// @file
+///
+/// @details
+/// This is the main Arduino project file.
+
+//
+// Local Includes
+//
+
+//
+// Local Definitions
+//
+
+//
+// Local Structures / Enumerations
+//
+
+//
+// Local Typedefs
+//
+
+//
+// Local Method Definitions
+//
+
+//
+// Local Global Variables
+//
+
+//
+// Code
+//
+void setup() {
+
+}
+
+void loop() {
+
+}
